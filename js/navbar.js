d72a138b79c136c6fa165471fb0bf5e87aa623dc
Michael Mangrobang <micky.jm4@gmail.com>
2018-03-20 09:18:47 -1000

    Testing Menu; trying to fix on other computers

--- js/navbar.js
+++ js/navbar.js
@@ -6,14 +6,14 @@ $(document).ready(function () {
 
     $(this).scrollTop(0);
 
-    $('nav.ui.fixed.menu').find('a').click(function(){
+    $('nav.ui.fixed.menu').find('a.item').click(function(){
         var href = $(this).attr('href');
         //var $anchor = $('#'+$href).offset();
         if (href == '#home') {
-            $('body').animate({ scrollTop: 0 }, 500);
+            $('html, body').animate({ scrollTop: 0 }, 500);
         } else {
             var anchor = $(href).offset();
-            $('body').animate({ scrollTop: anchor.top }, 500);
+            $('html, body').animate({ scrollTop: anchor.top }, 500);
         }
         return false;
     });
